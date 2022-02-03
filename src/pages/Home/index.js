76385d96f85a5c10da98fe695c8d0a009eda23c6
freaklucas <lucas.oliveira.freak@gmail.com>
2022-02-02 22:47:23 -0300

    setup de criação

---
+++ src/pages/Home/index.js
@@ -0,0 +1,22 @@
+import "./home.css";
+
+import { FiLink } from "react-icons/fi";
+
+export default function Home() {
+  return (
+    <div className="container-home">
+      <div className="logo">
+        <img src="/logo.png" alt="Sujeito link logo" />
+        <h1>Sujeito Link</h1>
+        <span>Cole seu link para encurtar 👇</span>
+      </div>
+      <div className="area-input">
+        <div>
+          <FiLink size={24} color="#FFF" />
+          <input placeholder="Cole aqui seu link..." />
+        </div>
+        <button>Encurtar Link</button>
+      </div>
+    </div>
+  );
+}
