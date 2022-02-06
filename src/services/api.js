2a78552e37d1ae75e3cd67c081593c75353a46cb
freaklucas <lucas.oliveira.freak@gmail.com>
2022-02-06 01:45:21 -0300

    add api

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+// 796574843113f70bbaa3720f9c693970f8906b60
+
+import axios from "axios";
+
+export const key = "796574843113f70bbaa3720f9c693970f8906b60";
+
+const api = axios.create({
+  baseURL: "https://api-ssl.bitly.com/v4/",
+  headers: {
+    Authorization: `Bearer ${key}`,
+  },
+});
+
+export default api;
