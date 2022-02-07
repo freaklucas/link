75345fb286cbb08108b8d435b36e72d0ed2c1aa5
freaklucas <lucas.oliveira.freak@gmail.com>
2022-02-06 23:57:24 -0300

    buscando na api

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ import axios from "axios";
 export const key = "d31291f371afb8f6a430e34c8504ff33ac789572";
 
 const api = axios.create({
-  baseURL: "https://api-ssl.bitly.com/v4/",
+  baseURL: "https://api-ssl.bitly.com/v4",
   headers: {
     Authorization: `Bearer ${key}`,
   },
